b51ad99cdbecd3ac8ff0a124d239e83a4c6aab37
Saiful-global <saifulislambappi01@gmail.com>
2026-05-06 22:38:22 +0600

    remove a className

--- src/Components/Shop/SortAndView.jsx
+++ src/Components/Shop/SortAndView.jsx
@@ -45,7 +45,7 @@ const SortAndView = () => {
         <ul className="font-jost font-medium text-sm leading-6 text-primary-black uppercase flex items-center gap-2.75 relative after:absolute after:top-0 after:-right-5.75 after:h-5.5 after:w-0.5 after:bg-[#E4E4E4] after:content-[''] pl-7.5">
           VIEW
           <li>2</li>
-          <li className='font-jost text-base font-medium text-primary-black relative after:content-[""] after:w-full after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300;'>3</li>
+          <li>3</li>
           <li>4</li>
         </ul>
       </div>
