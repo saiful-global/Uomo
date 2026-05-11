9b9e0dafa3e0ecdfb4b9f81798350d4ba18b030e
Saiful-global <saifulislambappi01@gmail.com>
2026-05-12 00:25:01 +0600

    add allProduct items center

--- src/Components/Shop/AllProducts.jsx
+++ src/Components/Shop/AllProducts.jsx
@@ -6,7 +6,7 @@ const AllProducts = ({items,view}) => {
   return (
     <section className='pb-12.5'>
         <Container>
-            <div className={`grid ${view === 2? "grid-cols-2": view === 3? "grid-cols-3": "grid-cols-4"} gap-x-7.5 gap-y-15`}>
+            <div className={`grid ${view === 2? "grid-cols-2": view === 3? "grid-cols-3": "grid-cols-4"} gap-x-7.5 gap-y-15 justify-items-center`}>
                 {items?.map((item)=>(
                 <Product key={item.id} item={item} />
                 ))}
