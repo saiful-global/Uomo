0ce678aeebbb32633e009452475190f90339531a
Saiful-global <saifulislambappi01@gmail.com>
2026-05-19 06:31:55 +0600

    complete fotter responsive sm device

--- src/Components/layout/Footer.jsx
+++ src/Components/layout/Footer.jsx
@@ -9,12 +9,12 @@ import { Link } from 'react-router'
 const Footer = () => {
   return (
     <>
-    <footer className='bg-bg-footer pt-25'>
+    <footer className='bg-bg-footer pt-12.25 md:pt-25'>
       <Container>
-        <div className='flex justify-between'>
-          <div>
+        <div className='flex flex-col md:flex-row justify-between'>
+          <div className='pb-12.5 md:pb-0'>
             <Link to="/">
-              <Image className="pb-11.25" src="/images/logo.png"></Image>
+              <Image className="pb-7.25 md:pb-11.25" src="/images/logo.png"></Image>
             </Link>
             <p className='font-jost font-normal text-sm leading-6 text-primary-black'>1418 River Drive, Suite 35 Cottonhall, CA 9622</p>
             <p className='font-jost font-normal text-sm leading-6 text-primary-black pb-9.25'>United States</p>
@@ -22,7 +22,7 @@ const Footer = () => {
             <p className='font-jost font-medium text-sm leading-6 text-primary-black'>+1 246-345-0695</p>
             
             {/* Social Icons  */}
-            <div className='pt-11.75 flex gap-9'>
+            <div className='pt-6.75 md:pt-11.75 flex gap-9'>
               <a href="">
                 <FaFacebookF className='hover:text-blue-500 cursor-pointer' />
               </a>
@@ -40,33 +40,37 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className='flex flex-col'>
-            <h3 className='font-jost font-medium text-[18px] text-primary-black pb-6.75'>COMPANY</h3>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">About Us</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Careers</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Affiliates</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Blog</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Contact Us</a>
+          {/* COMPANY and SHOP in one div for responsive manage */}
+          <div className='flex gap-26.25 md:gap-37.5 pb-8.5 md:pb-0'>
+            <div className='flex flex-col'>
+              <h3 className='font-jost font-medium text-[18px] text-primary-black pb-2.5 md:pb-6.75'>COMPANY</h3>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">About Us</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Careers</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Affiliates</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Blog</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Contact Us</a>
+            </div>
+            <div className='flex flex-col'>
+              <h3 className='font-jost font-medium text-[18px] text-primary-black pb-2.75 md:pb-6.75'>SHOP</h3>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[60%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">New Arrivals</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Accessories</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Men</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Women</a>
+              <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Shop All</a>
+            </div>
           </div>
+          {/* Help  */}
           <div className='flex flex-col'>
-            <h3 className='font-jost font-medium text-[18px] text-primary-black pb-6.75'>SHOP</h3>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[60%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">New Arrivals</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Accessories</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Men</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Women</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Shop All</a>
-          </div>
-          <div className='flex flex-col'>
-            <h3 className='font-jost font-medium text-[18px] text-primary-black pb-6.75'>HELP</h3>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[60%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Customer Service</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">My Account</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Find a Store</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Legal & Privacy</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Contact</a>
-            <a className='font-jost font-normal text-[14px] leading-10 text-primary-black relative after:content-[""] after:w-0 hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Gift Card</a>
+            <h3 className='font-jost font-medium text-[18px] text-primary-black pb-2.75 md:pb-6.75'>HELP</h3>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[60%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Customer Service</a>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">My Account</a>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Find a Store</a>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Legal & Privacy</a>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Contact</a>
+            <a className='font-jost font-normal text-sm leading-10 text-primary-black relative after:content-[""] after:w-0 md:hover:after:w-[40%] after:h-0.5 after:bg-primary-black after:absolute after:bottom-0 after:left-0 after:duration-300' href="">Gift Card</a>
           </div>
           {/* SUBSCRIBE div */}
-          <div className='flex flex-col w-82.5'>
+          <div className='flex flex-col w-82.5 pt-12.5 md:pt-0'>
             <h3 className='font-jost font-medium text-[18px] text-primary-black pb-6.75'>SUBSCRIBE</h3>
             <p className='font-jost font-normal text-[14px] text-primary-black'>Be the first to get the latest news about trends,</p>
             <p className='font-jost pb-3.75 font-normal text-[14px] text-primary-black'>promotions, and much more!</p>
@@ -83,13 +87,13 @@ const Footer = () => {
         </div>
 
         {/* light line divider */}
-        <div className='pt-22 pb-8.5'>
+        <div className='pt-12.5 md:pt-22 pb-6.75 md:pb-8.5'>
           <div className='max-w-full bg-[#CFCDCD] p-px'></div>
         </div>
 
         {/* End Text  */}
-        <div className='pb-7 flex justify-between'>
-          <div className='flex gap-2 items-center'>
+        <div className='pb-20.75 md:pb-7 flex flex-col md:flex-row justify-between'>
+          <div className='flex pb-4 md:pb-0 gap-2 items-center'>
             <h3 className='font-jost font-normal text-sm leading-6 text-primary-black flex items-center'>
               ©2026 Uomo | 
             </h3>
@@ -102,17 +106,25 @@ const Footer = () => {
               github.com/saiful-global
               </a>
           </div>
-          <div className='flex items-center'>
-            <h4 className='font-jost font-normal text-sm leading-6 text-secondColor pr-6.75'>Language</h4>
-            <h4 title="Only English is available" className='cursor-pointer font-jost font-normal text-sm leading-6 text-secondColor  hover:text-primary-black pr-4'>United Kingdom  |  English</h4>
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.49932 3.36078C7.22726 3.08872 6.77274 3.08872 6.49999 3.36078L0.206656 9.638C-0.0688853 9.91355 -0.0688853 10.3604 0.206656 10.6352C0.482197 10.9108 0.929749 10.9108 1.20529 10.6352L6.99997 4.85667L12.794 10.6359C13.0702 10.9115 13.517 10.9115 13.7933 10.6359C14.0688 10.3604 14.0688 9.91355 13.7933 9.6387L7.49932 3.36078Z" fill="black"/>
-            </svg>
-            <h4 className='font-jost font-normal text-sm leading-6 text-secondColor pr-5.75 pl-8'>Currency</h4>
-            <h4 title="Only $USD is available" className='cursor-pointer font-jost font-normal text-sm leading-6 text-secondColor  hover:text-primary-black pr-2.5'>$ USD</h4>
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.49932 3.36078C7.22726 3.08872 6.77274 3.08872 6.49999 3.36078L0.206656 9.638C-0.0688853 9.91355 -0.0688853 10.3604 0.206656 10.6352C0.482197 10.9108 0.929749 10.9108 1.20529 10.6352L6.99997 4.85667L12.794 10.6359C13.0702 10.9115 13.517 10.9115 13.7933 10.6359C14.0688 10.3604 14.0688 9.91355 13.7933 9.6387L7.49932 3.36078Z" fill="black"/>
-            </svg>
+          <div className='flex items-center flex-wrap gap-4 md:gap-0'>
+
+            {/* Language  */}
+            <div className='flex items-center'>
+              <h4 className='font-jost font-normal text-sm leading-6 text-secondColor pr-6.75'>Language</h4>
+              <h4 title="Only English is available" className='cursor-pointer font-jost font-normal text-sm leading-6 text-primary-black md:text-secondColor  md:hover:text-primary-black pr-4'>United Kingdom  |  English</h4>
+              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.49932 3.36078C7.22726 3.08872 6.77274 3.08872 6.49999 3.36078L0.206656 9.638C-0.0688853 9.91355 -0.0688853 10.3604 0.206656 10.6352C0.482197 10.9108 0.929749 10.9108 1.20529 10.6352L6.99997 4.85667L12.794 10.6359C13.0702 10.9115 13.517 10.9115 13.7933 10.6359C14.0688 10.3604 14.0688 9.91355 13.7933 9.6387L7.49932 3.36078Z" fill="black"/>
+              </svg>
+            </div>
+
+            {/* Currency  */}
+            <div className='flex items-center'>
+              <h4 className='font-jost font-normal text-sm leading-6 text-secondColor pr-5.75 md:pl-8'>Currency</h4>
+              <h4 title="Only $USD is available" className='cursor-pointer font-jost font-normal text-sm leading-6 text-primary-black md:text-secondColor  md:hover:text-primary-black pr-2.5'>$ USD</h4>
+              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.49932 3.36078C7.22726 3.08872 6.77274 3.08872 6.49999 3.36078L0.206656 9.638C-0.0688853 9.91355 -0.0688853 10.3604 0.206656 10.6352C0.482197 10.9108 0.929749 10.9108 1.20529 10.6352L6.99997 4.85667L12.794 10.6359C13.0702 10.9115 13.517 10.9115 13.7933 10.6359C14.0688 10.3604 14.0688 9.91355 13.7933 9.6387L7.49932 3.36078Z" fill="black"/>
+              </svg>
+            </div>
           </div>
         </div>
       </Container>
