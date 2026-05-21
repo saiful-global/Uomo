31e2f557db604b93221067376ae6a6e3d4a6d501
Saiful-global <saifulislambappi01@gmail.com>
2026-05-21 22:53:51 +0600

    image hide in sm device

--- src/Components/shop/ShopBanner.jsx
+++ src/Components/shop/ShopBanner.jsx
@@ -8,7 +8,7 @@ const ShopBanner = () => {
   return (
     <>
     <section>
-        <Image className="mx-auto w-full px-15 pb-9" src={ShopImage} alt="ShopImage"></Image>
+        <Image className="mx-auto w-full px-15 pb-9 hidden md:block" src={ShopImage} alt="ShopImage"></Image>
         <Container className="relative">     
           <h2 className='font-jost font-bold text-[90px] uppercase absolute bottom-43.75 left-0 text-transparent [-webkit-text-stroke:2px_#222222] opacity-40'>Jackets & Coats</h2>                                     
             <ul className='flex List_Item gap-7.75 w-full absolute bottom-33.75 left-0'>
