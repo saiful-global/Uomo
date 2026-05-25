d4712a679f82b7c35f70c4f5b698ee5b884052fc
Saiful-global <saifulislambappi01@gmail.com>
2026-05-25 22:20:35 +0600

    update sm device responsive shopBanner text & size

--- src/Components/shop/ShopBanner.jsx
+++ src/Components/shop/ShopBanner.jsx
@@ -7,11 +7,11 @@ import Container from '../ui/Container'
 const ShopBanner = () => {
   return (
     <>
-    <section>
+    <section className='mt-15 md:mt-0'>
         <Image className="mx-auto w-full md:px-15 pb-9" src={ShopImage} alt="ShopImage"></Image>
         <Container className="relative">     
-          <h2 className='font-jost font-bold text-[30px] md:text-[90px] uppercase absolute bottom-43.75 left-0 text-transparent [-webkit-text-stroke:2px_#222222] opacity-40'>Jackets & Coats</h2>                                     
-            <ul className='flex flex-wrap List_Item gap-7.75 w-full absolute bottom-0 md:bottom-33.75 left-0'>
+          <h2 className='font-jost font-bold text-[30px] md:text-[90px] uppercase absolute bottom-30 md:bottom-43.75 left-[10%] md:left-0 text-transparent [-webkit-text-stroke:2px_#222222] opacity-40'>Jackets & Coats</h2>                                     
+            <ul className='flex flex-wrap List_Item gap-4 md:gap-7.75 w-full absolute bottom-10 md:bottom-33.75 left-0'>
                 <li>#STAYHOME</li>
                 <li>NEW IN</li>
                 <li>JACKETS</li>
